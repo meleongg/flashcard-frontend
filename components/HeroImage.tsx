77d33c767f86574ec6e5b73fd38c71c639fac32c
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2025-05-31 23:01:10 -0700

    add: PWA setup

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function HeroImage() {
   return (
     <Image
-      src="/hero-image.png"
+      src="/hero.png"
       alt="Flashlearn application screenshot"
       width={600}
       height={400}
